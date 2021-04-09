053c937e7635f5598240e07ddf4da003c380f7d6
Miguel <mike.lo.123@hotmail.com>
2021-04-09 12:46:39 +0100

    Add New Ideas

--- src/main/java/org/academiadecodigo/hackathonbackend/daos/mock/IdeasDaoMock.java
+++ src/main/java/org/academiadecodigo/hackathonbackend/daos/mock/IdeasDaoMock.java
@@ -28,6 +28,18 @@ public class IdeasDaoMock implements IdeasDao {
         newIdea("Pick up trash from the sidewalk", "Those damn kids!", Mood.GOOD_HEART);
         newIdea("Go to the tallest point of your city", "That might be a mountain to hike, a new landmark to discover or just a friend of yours that's really tall", Mood.ADVENTURE);
         newIdea("Clean all the dishes", "They have been piling up, the time to do it is now!", Mood.NORMAL);
+
+        newIdea("Send your best meme about bears to NASA and wait for their invite to their next mission", "Maybe they're needing someone adventurous, that can think beyond what they have already achieved. And clearly you're the right person for that", Mood.ADVENTURE);
+        newIdea("Interrogate your grandma about what is a primary key", "Maybe a drawing will help", Mood.EVIL_GENIUS);
+        newIdea("Buy a van and go on a trip for 69 days", "Who says you don't have enough money? Save it well and enjoy a great trip!", Mood.ADVENTURE);
+        newIdea("Do a Hackathon for 36 hours", "doing an application in a limited time-frame with a team is an one-time experience, where you creativity bubbles.", Mood.ADVENTURE);
+        newIdea("Become a Internet meme", "Try reddit or 9GAG", Mood.ADVENTURE);
+        newIdea("Offer your help to construct a home for homeless people", "The best way to feel good is by helping others that need more than us", Mood.GOOD_HEART);
+        newIdea("Give a shower to a monkey", "What do you mean you never done it before yet?", Mood.GOOD_HEART);
+        newIdea("Live like a hermit for a hour", "put the tech to a side, contact with no one, don't even read a book. Just be with yourself for 60 minutes.", Mood.ADVENTURE);
+        newIdea("Adopt an unicorn", "or any other pet if you can't find one", Mood.ADVENTURE);
+        newIdea("Taste Korean Food", "The closest Korean restaurant is 2 miles away", Mood.NORMAL);
+        newIdea("Paint a wall", "Choose a design of your preference, buy some pencils and give wings to your imagination! Don't think about getting it perfect. Just get it done.", Mood.NORMAL);
     }
 
     public void newIdea(String title, String description, Mood mood) {
